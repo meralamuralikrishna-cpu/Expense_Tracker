1bac4b9429747261f2663793d762ab3c82ca4bd0
meralamuralikrishna-cpu <meralamuralikrishna@gmail.com>
2026-07-25 22:44:25 +0530

    Added Delete_Expense()

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ def Add_Expense():
         loaded_data = json.load(file)
 
 
-    id = input("Enter Id")
-    data = input("Enter Data")
-    amount = input("Enter Amount")
-    category = input("Enter Category")
-    description = input("Enter Description")
+    id = input("Enter Id: ").strip()
+    data = input("Enter Data: ").strip()
+    amount = input("Enter Amount: ".strip())
+    category = input("Enter Category: ").strip()
+    description = input("Enter Description: ").strip()
 
     data = {
         "id":id,
@@ -26,7 +26,7 @@ def View_Expense():
     with open("expenses.json","r") as file:
         loaded_data = json.load(file)
     
-    id = input("Enter Id to view the Expense")
+    id = input("Enter Id to view the Expense: ").strip()
     for i in range(0,len(loaded_data["expenses"])):
         if loaded_data["expenses"][i]["id"] == id:
             print(loaded_data["expenses"][i])
@@ -34,7 +34,32 @@ def View_Expense():
             
         else:
             print(f"No Id was Found By the {id}")
+            break
+
+def Delete_Expense():
+    id = input("Enter Id to Delete: ").strip()
+    with open("expenses.json","r") as file:
+        loaded_data = json.load(file)
+
+    for i in range(0,len(loaded_data["expenses"])):
+        if loaded_data["expenses"][i]["id"] == id:
+            del loaded_data["expenses"][i]
+            print(f"Successfully Deleted Id:{id}")
+            break
+        else:
+            print(f"No Id was Found by the {id}")
+            break
+
+    with open("expenses.json",'w') as file:
+        json.dump(loaded_data,file,indent=4)
+
+Delete_Expense()
+
+
+
+
+
+
             
             
 
-View_Expense()
